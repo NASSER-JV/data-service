eea48aad8ed3465fb63b89c14d55108bd1cb2eb9
João Vitor Goncalves Pina <joaovitorpinag@hotmail.com>
2021-11-11 02:23:35 -0300

    Correcão da baseDir do MikroOrm
    Signed-off-by: João Vitor Goncalves Pina <joaovitorpinag@hotmail.com>

--- src/configurations/mikro-orm.config.ts
+++ src/configurations/mikro-orm.config.ts
@@ -1,9 +1,9 @@
 import { IDatabaseDriver, Options } from '@mikro-orm/core';
 
 const dbConfig: Options<IDatabaseDriver> = {
-  baseDir: __dirname,
-  entities: ['../data/entities'],
-  entitiesTs: ['../data/entities'],
+  baseDir: './data-service',
+  entities: ['../dist/data/entities'],
+  entitiesTs: ['../src/data/entities'],
   dbName: process.env.DB_NAME,
   type: 'postgresql',
   password: process.env.DB_PASS,
