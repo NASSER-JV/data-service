f14767b04fa497cd72d2a13a0c7600ed684c4f16
NsRFrh <nasserfarhatt@gmail.com>
2021-11-10 22:46:02 -0300

    Adicionando a utilização de .env e as entidades do banco.

---
+++ src/entities/juncoes.ts
@@ -0,0 +1,20 @@
+import {Entity, IdentifiedReference, OneToOne, PrimaryKey, Property} from "@mikro-orm/core";
+import {Empresa} from "./empresa";
+
+
+@Entity()
+export class Juncoes {
+
+    @PrimaryKey({ unique: true })
+    id!: number;
+
+    @Property()
+    dataInicio!: Date;
+
+    @Property()
+    dataFim!: Date;
+
+    @OneToOne(() => Empresa)
+    empresa: IdentifiedReference<Empresa>;
+
+}
---
+++ src/entities/noticias.ts
@@ -0,0 +1,26 @@
+import {Entity, Enum, IdentifiedReference, OneToOne, PrimaryKey, Property} from "@mikro-orm/core";
+import {SentimentalEnum} from "@/data/sentimental.enum";
+import {Empresa} from "./Empresa";
+
+
+@Entity()
+export class Noticias {
+
+    @PrimaryKey({ unique: true })
+    url!: string;
+
+    @Property()
+    titulo!: string;
+
+    @Property()
+    corpo!: string;
+
+    @Property()
+    date: Date;
+
+    @Enum()
+    analise: SentimentalEnum.Neutro;
+
+    @OneToOne(() => Empresa)
+    empresa: IdentifiedReference<Empresa>;
+}
