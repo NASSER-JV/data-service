e965673fec8252f7e9ac643c9be26b6bbe2ab670
NsRFrh <nasserfarhatt@gmail.com>
2021-11-11 22:59:58 -0300

    Adicionando os middlewares de auth.

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,14 @@
+import { Injectable, NestMiddleware, UnauthorizedException } from '@nestjs/common';
+import passport from 'passport';
+@Injectable()
+export class AuthMiddleware implements NestMiddleware {
+  use(req: any, res: any, next: () => void) {
+    passport.authenticate('headerapikey', { session: false, failureRedirect: '/' }, (value) => {
+      if (value) {
+        next();
+      } else {
+        throw new UnauthorizedException();
+      }
+    })(req, res, next);
+  }
+}
